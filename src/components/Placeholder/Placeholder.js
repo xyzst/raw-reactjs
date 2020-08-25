2d326c751c871dd74b2373a3529fd49a9fde0e05
Darren Rambaud <xyzst@users.noreply.github.com>
2020-08-24 22:03:42 -0500

    Fix compilation issue

--- src/components/Placeholder/Placeholder.js
+++ src/components/Placeholder/Placeholder.js
@@ -1,28 +1,28 @@
-import React, {Component} from 'react';
-import styled from 'styled-components';
+import React, { Component } from "react";
+import styled from "styled-components";
 import AnotherPlaceholder from "../AnotherPlaceholder/AnotherPlaceholder";
 
 class Placeholder extends Component {
-
-  const Button = styled.a`
-  /* This renders the buttons above... Edit me! */
-  display: inline-block;
-  border-radius: 3px;
-  padding: 0.5rem 0;
-  margin: 0.5rem 1rem;
-  width: 11rem;
-  background: transparent;
-  color: white;
-  border: 2px solid white;
+  Button = styled.a`
+    /* This renders the buttons above... Edit me! */
+    display: inline-block;
+    border-radius: 3px;
+    padding: 0.5rem 0;
+    margin: 0.5rem 1rem;
+    width: 11rem;
+    background: transparent;
+    color: white;
+    border: 2px solid white;
   `;
 
   render() {
     return (
       <div>
         <Button>Rawr</Button>
-        <AnotherPlaceholder/>
+        <AnotherPlaceholder />
         {this.props.handler()}
-      </div>)
+      </div>
+    );
   }
 }
 
